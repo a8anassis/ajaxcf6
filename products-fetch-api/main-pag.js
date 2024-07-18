833936f7f00a8e40061a1e78ebb3b1fb09cd97ae
Th. Androutsos <a8anassis@gmail.com>
2024-07-18 20:58:55 +0300

    paginatio added

---
+++ products-fetch-api/main-pag.js
@@ -0,0 +1,50 @@
+let itemsPerPage = 5
+let currentPage = 1
+let totalPages = 0
+let allProducts = []
+
+
+$(function() {
+    fetchData()
+    .then(products => {
+        allProducts = products
+        renderItems(products, currentPage)
+        renderPagination(products)
+    })
+    .catch(error => console.log("Failed to fetch products", error))
+
+
+    $('#pagination').on('click', 'a', function(e) {
+        e.preventDefault()  // prevent default link behavior
+        currentPage = $(this).data('page')
+        renderItems(allProducts, currentPage)
+        renderPagination(allProducts)
+    })
+})
+
+function renderItems(products, page) {
+    $('#prod-list').empty()
+    const startIndex = (page - 1) * itemsPerPage
+    const endIndex = Math.min(startIndex + itemsPerPage, products.length)
+
+    const productsHTML = products
+                                .slice(startIndex, endIndex)
+                                .map(product => 
+                                    `<div><strong>Name: </strong>${product.name}</div>
+                                    <div><strong>Price: </strong>${product.price}</div>
+                                    <div><strong>Type: </strong>${product.type}</div><hr>`
+                                )
+                                .join('');
+
+    $('#prod-list').html(productsHTML)
+}
+
+function renderPagination(products) {
+    totalPages = Math.ceil(products.length / itemsPerPage)
+    $('#pagination').empty()
+    for (let i = 1; i <= totalPages; i++) {
+        const activeClass = i === currentPage ? 'active' : ''
+        $('#pagination').append(`<li><a href="#" class="${activeClass}" data-page="${i}">${i}</a></li>`)
+    } 
+}
+
